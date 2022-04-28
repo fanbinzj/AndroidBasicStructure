c6e0dd5ad99ba9d62048a833fc5a99411b077449
binf <binfannz@gmail.com>
2022-04-28 13:46:06 +1200

    initial the project

---
+++ orange/src/main/java/co/nz/genesis/singlerepo1/MainActivity.kt
@@ -0,0 +1,17 @@
+package co.nz.genesis.singlerepo1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import co.nz.genesis.singlerepo1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
